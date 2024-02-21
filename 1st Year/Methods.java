4477c3c531ffc96a61c6e4719d72b16a21aff2ee
WreckItTags <148003711+WreckItTags@users.noreply.github.com>
2024-02-21 12:54:34 +0800

    Methods.java

---
+++ 1st Year/Methods.java
@@ -0,0 +1,61 @@
+// Binua, Ralph Gabriel B. 1BSIT-5 Inter Prog
+
+import java.util.Scanner;
+
+public class Ch7_PrExercise3 {
+
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter First Number: ");
+        int numOne = input.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int numTwo = input.nextInt();
+        
+        int twoNumber = one(numOne,numTwo);
+        
+        System.out.println(twoNumber);
+
+        System.out.print("Enter Third number: ");
+        int numThree = input.nextInt();
+
+        System.out.print("Enter Fourth number: ");
+        int numFour = input.nextInt();
+
+        double oneNumber = two(numThree,numFour);
+
+        System.out.print(oneNumber);
+
+    }
+    
+    public static int one (int x, int y){
+          if(x>y){
+              int oneReturn = x+y;
+              return oneReturn;
+          }
+          else{
+              int twoReturn = (x-2)*y;
+              return twoReturn;
+          }
+
+    }
+    
+    public static double two(int x, double a){
+        int first;
+        double z = x;
+        
+       z = z+a;
+
+       first = one(6,8);
+
+       first = first+x;
+
+       if(z>first){
+           return z;
+       }else{
+           return 2*first-z;
+       }
+
+    }
+}
