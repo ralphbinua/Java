5036d207ed803a3424e0b8e397c6b739cd49cea6
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-10-09 00:38:35 +0800

    Create Display.java

---
+++ 2nd Year/Display.java
@@ -0,0 +1,12 @@
+public class Display {
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.println("Array contents:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
