157bc75882b4a47c58ba539b8d1d6e59fbe5fa64
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-10-09 00:50:29 +0800

    Create Linear.java

---
+++ 2nd Year/BigONotation/Linear.java
@@ -0,0 +1,17 @@
+public class Linear {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+        int result = linearSearch(array, target); // O(n) - takes time proportional to the input size
+        System.out.println(result);
+    }
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
