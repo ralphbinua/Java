a045fa00e4b0d6d98e7603d12dfa519f7dedbf31
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-10-09 01:19:00 +0800

    Create Exponential.java

---
+++ 2nd Year/BigONotation/Exponential.java
@@ -0,0 +1,11 @@
+public class Exponential {
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5; 
+        System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
+    }
+}
