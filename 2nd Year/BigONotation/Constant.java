c8b778a78c0b4edbfd0a3e67bbe3791809c7981a
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-10-09 00:45:56 +0800

    Update Constant.java

--- 2nd Year/BigONotation/Constant.java
+++ 2nd Year/BigONotation/Constant.java
@@ -1,1 +1,8 @@
-
+public class ConstantTime {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int result = x + y; // O(1) - takes the same time regardless of the input size
+        System.out.println(result);
+    }
+}
