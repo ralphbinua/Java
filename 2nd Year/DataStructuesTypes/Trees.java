619683efe5a66ecdd9ba532b4b1cf9ae7397a68f
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-10-09 01:52:51 +0800

    Create Trees.java

---
+++ 2nd Year/DataStructuesTypes/Trees.java
@@ -0,0 +1,33 @@
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    TreeNode root;
+
+    void inOrder(TreeNode node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+}
+
+public class TreeExample {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+
+        System.out.print("In-order traversal: ");
+        tree.inOrder(tree.root);  // Output: 2 1 3
+    }
+}
