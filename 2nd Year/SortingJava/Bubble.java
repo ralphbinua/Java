d88167f2116123fa48a0fd5b359023b83a6408d8
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-11-14 14:33:09 +0800

    Update Bubble.java

--- 2nd Year/SortingJava/Bubble.java
+++ 2nd Year/SortingJava/Bubble.java
@@ -1,23 +1,29 @@
 package SortingJava;
 
-public class Bubble {
-    public void BubbleSort(int[] array) {
-        int n = array.length;
-        boolean swapped;
+import java.util.Arrays;
 
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+public class Bubble {
+    public static int[] BubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean swapped = false;
+            System.out.println("Pass " + (i + 1) + ":");
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     swapped = true;
+                    System.out.println("Swapped " + arr[j + 1] + " and " + arr[j] + ": " + arrayToString(arr));
                 }
             }
             if (!swapped) {
                 break;
             }
         }
+        return arr;
+    }
+    private static String arrayToString(int[] arr) {
+        return Arrays.toString(arr);
     }
 }
