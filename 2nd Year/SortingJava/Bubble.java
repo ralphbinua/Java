13843bf378a474b7e17bdd2fb7ffa071a28cb7eb
Ralph Binua <148003711+ralphbinua@users.noreply.github.com>
2024-11-14 14:05:10 +0800

    Create Bubble.java

---
+++ 2nd Year/SortingJava/Bubble.java
@@ -0,0 +1,23 @@
+package SortingJava;
+
+public class Bubble {
+    public void BubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
