b4f254af36155b99f5c0dc2dd43397f4a624e559
Ralph Binua <binua.ralphgabrielb@gmail.com>
2025-04-30 00:45:56 +0800

    vendo

--- 2nd Year/VendoCoupon/Coupon.java
+++ 2nd Year/VendoCoupon/Coupon.java
@@ -2,4 +2,36 @@ package VendoCoupon;
 
 public class Coupon{
     private String CouponName;
+    private boolean isUse;
+    private int couponDiscount;
+
+    public Coupon(String CouponName, int couponDiscount, boolean isUse){
+        this.CouponName = CouponName;
+        this.couponDiscount = couponDiscount;
+        this.isUse = isUse;
+    }
+
+    public void setCouponName(String CouponName){
+        this.CouponName = CouponName;
+    }
+
+    public void setcouponDiscount(int couponDiscount){
+        this.couponDiscount = couponDiscount;
+    }
+
+    public void setIsUse(boolean isUse){
+        this.isUse = true;
+    }
+
+    public String getCouponName(){
+        return CouponName;
+    }
+
+    public int getcouponDiscount(){
+        return couponDiscount;
+    }
+
+    public boolean getIsUse(){
+        return isUse;
+    }
 }
--- 2nd Year/VendoCoupon/Main.java
+++ 2nd Year/VendoCoupon/Main.java
@@ -1,8 +1,35 @@
 package VendoCoupon;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args){
 
-        System.out.print("Hello");
+        Scanner input = new Scanner(System.in);
+         Coupon neu2024 = new Coupon("neu2024", 15, false);
+         int price = 20;
+
+         System.out.println();
+         System.out.println("Total price is: " + price);
+         System.out.print("Do you have a voucher code? (y/n): ");
+         String choice = input.nextLine();
+
+         if (choice.equals("y")) {
+             System.out.print("Enter voucher code: ");
+             String vCode = input.nextLine();
+
+             if (vCode.equals(neu2024.getCouponName())) {
+                 if (!neu2024.getIsUse()) {
+                     int discountedPrice = price - neu2024.getcouponDiscount();
+                     neu2024.setIsUse(true); // Mark the coupon as used
+                     System.out.println("Discounted price is: " + discountedPrice);
+                 } else {
+                     System.out.println("The coupon has already been used, please try another.");
+                 }
+             } else {
+                 System.out.println("There is no such coupon available.");
+             }
+         } else {
+             System.out.println("Final price is: " + price);
+         }
     }
 }
